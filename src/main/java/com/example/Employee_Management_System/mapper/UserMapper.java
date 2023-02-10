310f506b56857b5edb12e042ca23fe798e99da52
Hai Nguyen <bongnghin5@gmail.com>
2023-02-10 08:54:00 +0700

    add db config

--- src/main/java/com/example/Employee_Management_System/mapper/UserMapper.java
+++ src/main/java/com/example/Employee_Management_System/mapper/UserMapper.java
@@ -1,7 +1,13 @@
 package com.example.Employee_Management_System.mapper;
 
+import com.example.Employee_Management_System.domain.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
+    @Insert("INSERT INTO user (first_name, last_name, password, email, role, avatar, is_locked) VALUES (#{username}, #{password}, #{email}, #{role}, #{avatar}, #{isLocked})")
+    void save(User user);
+
+
 }
--- src/main/java/com/example/Employee_Management_System/repository/UserRepository.java
+++ src/main/java/com/example/Employee_Management_System/repository/UserRepository.java
@@ -1,16 +1,23 @@
 package com.example.Employee_Management_System.repository;
 
 import com.example.Employee_Management_System.domain.User;
+import com.example.Employee_Management_System.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
+public class UserRepository {
+    @Autowired
+    private UserMapper userMapper;
+
     // ToDo: save, update, find, delete, paging
-    User save(User user);
-    User modify(User user);
-    Optional<User> findByUsername(String username);
-    void deleteByUsername(String username);
-    boolean existsByUsername(String email);
+    public void save(User user) {
+        userMapper.save(user);
+    }
+//    User modify(User user);
+//    Optional<User> findByUsername(String username);
+//    void deleteByUsername(String username);
+//    boolean existsByUsername(String email);
 }
