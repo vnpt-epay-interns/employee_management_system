9930a74ad5ea79aa87b5fbb7f4eeeb3d7efe616f
Vũ Đỗ <88678046+ImVux21@users.noreply.github.com>
2023-02-24 16:01:36 +0700

    remove MyBatisConfig

--- src/main/java/com/example/Employee_Management_System/EmployeeManagementSystemApplication.java
+++ src/main/java/com/example/Employee_Management_System/EmployeeManagementSystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.Employee_Management_System.mapper")
 public class EmployeeManagementSystemApplication {
 
 	public static void main(String[] args) {
