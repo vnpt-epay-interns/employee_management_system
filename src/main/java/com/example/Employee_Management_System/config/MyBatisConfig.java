fca9d05b26ba6dc87d0a0bb6cb12236979e349c3
Vũ Đỗ <88678046+ImVux21@users.noreply.github.com>
2023-02-24 16:48:58 +0700

    try to

--- src/main/java/com/example/Employee_Management_System/config/MyBatisConfig.java
+++ src/main/java/com/example/Employee_Management_System/config/MyBatisConfig.java
@@ -12,21 +12,21 @@ import javax.sql.DataSource;
 @Configuration
 @MapperScan("com.example.Employee_Management_System.mapper")
 public class MyBatisConfig {
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://containers-us-west-141.railway.app:5774/railway");
-        dataSource.setUsername("root");
-        dataSource.setPassword("yx7jx47vP6OCw7UEEg6E");
-        return dataSource;
-    }
-
-    @Bean
-    public SqlSessionFactoryBean sqlSessionFactoryBean() {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
-        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
-        return sqlSessionFactoryBean;
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://containers-us-west-141.railway.app:5774/railway");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("yx7jx47vP6OCw7UEEg6E");
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    public SqlSessionFactoryBean sqlSessionFactoryBean() {
+//        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+//        sqlSessionFactoryBean.setDataSource(dataSource());
+//        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+//        return sqlSessionFactoryBean;
+//    }
 }
