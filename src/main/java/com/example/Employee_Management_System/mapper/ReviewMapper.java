4200dbc250b8aeabf1e8f560aaabaead392ed527
Vu Do <vuodoan@gmail.com>
2023-04-25 15:16:31 +0700

    order for taskForProject

--- src/main/java/com/example/Employee_Management_System/mapper/ReviewMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.Employee_Management_System.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public class ReviewMapper {
-}
--- src/main/java/com/example/Employee_Management_System/service/impl/ReviewServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.Employee_Management_System.service.impl;
-
-import com.example.Employee_Management_System.service.ReviewService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ReviewServiceImpl implements ReviewService {
-}
